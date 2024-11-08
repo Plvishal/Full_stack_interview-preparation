29c4b3d20c991688e698460beaff3348cbcc35ff
Vishal <vishalpal1501@gmail.com>
2024-11-08 22:38:26 +0530

    create consumer

--- react_context_toolkit/src/context/Counter.jsx
+++ react_context_toolkit/src/context/Counter.jsx
@@ -1,9 +1,11 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
 // 1. create context
 const CounterContext = createContext();
+// 3. create custom hooks for consume contex
+export const useCounterContext = () => useContext(CounterContext);
 
-// 
+//
 
 // 2. Provide context
 export const CounterProvider = ({ children }) => {
