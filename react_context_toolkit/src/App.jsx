982d71f45fbf735c14aa79cb7613d6c27935f82f
Vishal <vishalpal1501@gmail.com>
2024-11-09 08:45:42 +0530

    Remove first  context and in this we are implement user login using user login  and logout

--- react_context_toolkit/src/App.jsx
+++ react_context_toolkit/src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
-import { useCounterContext } from './context/Counter';
 
 function App() {
-  const { count } = useCounterContext();
   return (
     <>
-      <h1>Hello : {count}</h1>
+      <h1>Hello</h1>
     </>
   );
 }
