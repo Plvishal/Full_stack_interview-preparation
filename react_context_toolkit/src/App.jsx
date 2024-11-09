42d5f0d2d284e818139426249bf318c5b0ccea80
Vishal <vishalpal1501@gmail.com>
2024-11-09 09:16:07 +0530

    create some pages for login and show admin home

--- react_context_toolkit/src/App.jsx
+++ react_context_toolkit/src/App.jsx
@@ -1,27 +1,9 @@
 import './App.css';
-import { useUser } from './context/UserContext';
 
 function App() {
-  const { user, login, logout } = useUser();
-  const handleLogin = () => {
-    login('vishalpal1501@gmail.com', 'vishal123');
-  };
-  const handleLogout = () => {
-    logout();
-  };
   return (
     <>
-      {user ? (
-        <div>
-          <h1> Welcome to the : {user.email}</h1>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <h2>Please Login</h2>
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
+      <h1>khih</h1>
     </>
   );
 }
